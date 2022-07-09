ca46768b0f6fc6674ab238e3efcd9c36f42b2925
mkaruc <fatihmehmetkaruc@gmail.com>
2022-07-09 23:15:57 +0300

    haveanerror

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
-import Card from './components/card/Card';
-import data from './util/data';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
-function App() {
-  console.log(data);
+const App = () => {
+  const[user,setUser]=useState([]);
+
+  useEffect(() => {
+  
+const userList=async()=>{
+  try{
+    const {user: response} = await axios.get("https://randomuser.me/api/");
+    setUser(response);
+  }catch(error){
+    console.error(error.message);
+  }}
+  userList();
+ }, []);
+
+
   return (
-    <>
-      {data.map((item) => {
-        //!Destr
-        const { id, img, btnName, dil } = item;
-        return <Card key={id} img={img} btnName={btnName} dil={dil} />;
-      })}
-    </>
-  );
+    <div className="userCard">
+      {user.map((item,index)=> (
+      <div key={index} className='container'>
+        <div className='profileHeader'>
+          <img src={item.picture.medium} alt="" />
+          <h2>{item.name}</h2>
+        </div>
+        <div className='email'>
+          <img src="./assets/email.svg" alt="" />
+          <h3>{item.email}</h3>
+        </div>
+        <div className='phone'>
+          <img src="./assets/phone.svg" alt="" />
+          <h3>{item.phone}</h3>
+        </div>
+        <div className='location'>
+        <img src="./assets/location.svg" alt="" />
+          <h3>{item.location}</h3>
+        </div>
+        <div className='age'><h3>Age: {item.dob.age}</h3></div>
+        <div className='register'><h3>Register Date: {item.registered.date.substr(0, 10)}</h3></div>
+      </div>))}
+      <button onClick={user}>Random User</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
