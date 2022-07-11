131aeba9aa9230b922158623eeea4d8a109ad2ef
mkaruc <fatihmehmetkaruc@gmail.com>
2022-07-11 15:14:16 +0300

    endoftheproject

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
 import { useEffect, useState } from 'react';
-import axios from "axios";
+import emailIcon from "./assets/email.svg";
+import phoneIcon from "./assets/phone.svg";
+import locationIcon from "./assets/location.svg";
+import "./App.css";
 
 const App = () => {
   const[user,setUser]=useState();
 
   useEffect(() => {
+    userList();
+  }, []);
   
-const userList=async()=>{
-  try{
-    const {data} = await axios.get("https://randomuser.me/api/");
-    setUser(data);
-  }catch(error){
-    console.log(error);
-  }}
-  
-  userList();
- }, []);
+const userList=()=>{
+  fetch("https://randomuser.me/api/")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data.results);
+        console.log(data.results);
+      });
+  };
 
 
   return (
     <div className="userCard">
-      {user.map((item,index)=> (
+      {user?.map((item,index)=> (
       <div key={index} className='container'>
         <div className='profileHeader'>
-          <img src={item.picture.medium} alt="" />
-          <h2>{item.name}</h2>
+          <img src={item.picture.medium} alt="" className='pp'/>
+          <h2>{item.name.title} {item.name.first} {item.name.last}</h2>
         </div>
         <div className='email'>
-          <img src="./assets/email.svg" alt="" />
+          <img src={emailIcon} alt="" className="icon"/>
           <h3>{item.email}</h3>
         </div>
         <div className='phone'>
-          <img src="./assets/phone.svg" alt="" />
-          <h3>{item.phone}</h3>
+          <img src={phoneIcon} alt="" className="icon"/>
+          <h3>{item.cell}</h3>
         </div>
         <div className='location'>
-        <img src="./assets/location.svg" alt="" />
-          <h3>{item.location}</h3>
+        <img src={locationIcon} alt="" className="icon"/>
+          <h3>{item.location.city} - {item.location.country}</h3>
         </div>
         <div className='age'><h3>Age: {item.dob.age}</h3></div>
         <div className='register'><h3>Register Date: {item.registered.date.substr(0, 10)}</h3></div>
       </div>))}
-      <button onClick={user}>Random User</button>
+      <button onClick={userList}>Random User</button>
     </div>
   )
 }
